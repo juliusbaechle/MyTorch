3431b9eb06395cf08577f82c05181d9cd5b79428
Julius Bächle <juliusbaechle@users.noreply.github.com>
2026-03-06 19:36:56 +0100

    bugfix topo_sort

--- mytorch/topo_sort.py
+++ mytorch/topo_sort.py
@@ -1,14 +1,17 @@
-def build_topo(tensor, visited=set(), topo_order=[]):
+def build_topo(tensor, visited=None, topo_order=None):
+    if visited is None:
+        visited = set()
+
+    if topo_order is None:
+        topo_order = []
+
     if id(tensor) in visited:
         return topo_order
     
     visited.add(id(tensor))
 
-    # build_topo for each parent
-    for parent_ref in getattr(tensor, "_parents", ()):
-        parent = parent_ref()
-        if parent is not None:
-            build_topo(parent, visited, topo_order)
+    for p_ref in tensor._parents:
+        build_topo(p_ref(), visited, topo_order)
 
     topo_order.append(tensor)
     return topo_order
